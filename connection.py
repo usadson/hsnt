f302aae3a0c9bf8fa3828974ecbab94ce4caa88f
Tristan <code@thewoosh.me>
2020-06-29 16:25:08 +0200

    [Connection] Improve file (pylint)

--- connection.py
+++ connection.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-# Copyright (C) <year>  <name of author>
+# Copyright (C) 2020, Tristan <42b202ec-1be9-4f66-8098-c506a1b4dfa2@anonaddy.me>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,18 +14,32 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+"""
+FileInfo: Simple connection class
+"""
+
 import socket
 
 class Connection:
+    """
+    FileInfo: Simple connection class
+    """
     def __init__(self, host, port):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((host, port))
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((host, port))
 
     def __del__(self):
-        self.s.close()
+        self.socket.close()
 
     def send(self, string):
-        self.s.sendall(bytes(string, 'utf-8'))
+        """
+        Sends a string over the socket, encoded as UTF-8.
+        """
+        self.socket.sendall(bytes(string, 'utf-8'))
 
     def readline(self):
-        return self.s.makefile().readline()
+        """
+        Reads a line from the socket.
+        NOTE: This includes the '\n' character, but the '\r' character gets omitted.
+        """
+        return self.socket.makefile().readline()
